e63091d640a358ec86f091594f44e757c8345eed
andrei.190582 <andrei.190582@192.168.3.64>
2014-11-05 19:28:57 -0200

    Adicionando OutraClasse

---
+++ src/br/com/yaw/git/App/OutraClasse.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.yaw.git.App;
+
+/**
+ *
+ * @author andrei.190582
+ */
+public class OutraClasse {
+    
+}
