1e46bf495e7d2ea0459d8ce022020674cd961127
andrei.190582 <andrei.190582@192.168.3.64>
2014-11-05 19:49:03 -0200

    Fazendo Alterações no App.java

--- src/br/com/yaw/git/App/App.java
+++ src/br/com/yaw/git/App/App.java
@@ -14,7 +14,7 @@ public class App {
         
         System.out.println("Utilizando o git através do NetBeans 8.0");
         int n =300;
-        System.out.prinln("Bordignon Commitr the walsh");
+        System.out.println("Bordignon Commit the walsh");
     }
     
 }
